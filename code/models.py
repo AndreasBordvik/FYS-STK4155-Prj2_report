279e36f58833326e3e36fed775cf5a87ef1c41e4
Are Frode Kvanum <are.frode.kvanum@gmail.com>
2021-10-27 15:24:58 +0200

    More changes to forwar_prop, asserted with XOR for Fixed layer

--- code/models.py
+++ code/models.py
@@ -1,6 +1,69 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
+# Activation functions
+def relu(x):
+    return np.maximum(0,x)
+
+def sigmoid(x):
+    return np.exp(x) / (1 + np.exp(x))
+
+def binary_classifier(x):
+    return np.where(x>=0,1,0)
+
+class Fixed_layer:
+    def __init__(self, nbf_inputs : int, nbf_outputs : int, weights, bias, activation_function=lambda x: x):
+        self.input = nbf_inputs
+        self.output = nbf_outputs
+        self.activation = activation_function
+        self.weights = weights#np.random.rand(nbf_inputs, nbf_outputs) # TODO: include possible negative weight initialization
+        self.bias = bias#np.random.rand(1, nbf_outputs) # TODO: include possible negative weight initialization
+        self.accumulated_gradient = np.zeros_like(self.weights)
+
+    def forward_prop(self, input_ : np.ndarray) -> np.ndarray:
+        z = (input_ @ self.weights) + self.bias
+        a = self.activation(z)
+        return a, self.weights, self.bias
+
+class Layer:
+    def __init__(self, nbf_inputs : int, nbf_outputs : int, activation_function=lambda x: x):
+        self.input = nbf_inputs
+        self.output = nbf_outputs
+        self.activation = activation_function
+        self.weights = np.random.rand(nbf_inputs, nbf_outputs) -0.5 # TODO: include possible negative weight initialization
+        self.bias = np.random.rand(1, nbf_outputs) -0.5# TODO: include possible negative weight initialization
+        self.accumulated_gradient = np.zeros_like(self.weights)
+
+    def forward_prop(self, input_ : np.ndarray) -> np.ndarray:
+        z = (input_ @ self.weights) + self.bias
+        a = self.activation(z)
+        return a, self.weights, self.bias
+
+    def backward_prop(self, X, y):
+        h = self.forward_prop(X)
+        error = y - y_hat 
+
+        # TODO: DO backprop
+
+class NeuralNetwork:
+    def __init__(self):
+        self.sequential_layers = []
+        
+
+    def add_layer(self, layer: Layer):
+        self.sequential_layers.append(layer)
+
+    def predict(self, input_):
+        X = input_.copy()
+        for layer in self.sequential_layers:
+            print(X, '\n')
+            X, w, b = layer.forward_prop(X)
+            print(w, '\n')
+            print(b, '\n')
+
+
+
+        return X
 
 class own_LinRegGD():
     def __init__(self):
@@ -31,6 +94,10 @@ class own_LinRegGD():
     def add_bias(self, x):
         # Bias element = 1 is inserted at index 0
         return np.insert(x, 0, 1, axis=1)
+
+
+
+
 
 
 if __name__ == '__main__':
